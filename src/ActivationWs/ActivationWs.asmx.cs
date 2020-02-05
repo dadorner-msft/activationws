fdbc48926ee51329a5afb9f10de97f35f21a49f8
Daniel Dorner <github@ddorner.de>
2020-02-05 23:32:42 +0100

    Update ActivationWs.asmx.cs

--- src/ActivationWs/ActivationWs.asmx.cs
+++ src/ActivationWs/ActivationWs.asmx.cs
@@ -14,12 +14,13 @@ namespace ActivationWs
     [System.ComponentModel.ToolboxItem(false)]
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
     // [System.Web.Script.Services.ScriptService]
-    public class ActivationWs : System.Web.Services.WebService
+    public class ActivationService : System.Web.Services.WebService
     {
 
         [WebMethod]
-        public string AcquireConfirmationId(string installationId, string extendedProductId) {
-            string confirmationId = Activation.CallWebService(installationId, extendedProductId);
+        public string AcquireConfirmationId(string installationId, string extendedProductId)
+        {
+            string confirmationId = ActivationHelper.CallWebService(installationId, extendedProductId);
             return confirmationId;
         }
     }
