5d69151578ba83caefe31047bbbdb438d1ff02c6
Daniel Dorner <github@dorner-teo.de>
2020-06-21 12:46:34 +0200

    Update Default.aspx.cs

--- src/ActivationWs/Default.aspx.cs
+++ src/ActivationWs/Default.aspx.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace ActivationWs
 {
@@ -13,5 +8,6 @@ namespace ActivationWs
         {
 
         }
+
     }
 }
