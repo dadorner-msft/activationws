ab712a066f612a1b9c5c286b4e44733fb2a74e39
Daniel Dorner <github@dorner-teo.de>
2020-08-03 21:15:35 +0200

    Update Default.aspx.cs

--- src/ActivationWs/Default.aspx.cs
+++ src/ActivationWs/Default.aspx.cs
@@ -4,10 +4,32 @@ namespace ActivationWs
 {
     public partial class WebForm : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
+        protected void Page_Load(object sender, EventArgs e) {
+            if (RadioButtonList1.SelectedIndex == 0) {
+                InstallationId.Enabled = true;
 
+            } else {
+                InstallationId.Enabled = false;
+            }
         }
 
+        protected void Submit_Click(Object sender, EventArgs E) {
+            try {
+                using (ActivationService activationService = new ActivationService()) {
+                    if (RadioButtonList1.SelectedIndex == 0 && !((string.IsNullOrWhiteSpace(InstallationId.Text)) || (string.IsNullOrWhiteSpace(ExtendedProductId.Text)))){
+                        Result.Text = string.Format("The Confirmation ID is: <b>{0}</b>", activationService.AcquireConfirmationId(InstallationId.Text, ExtendedProductId.Text));
+
+                    } else if (RadioButtonList1.SelectedIndex == 1 && !(string.IsNullOrWhiteSpace(ExtendedProductId.Text))) {
+                        Result.Text = string.Format("You have <b>{0}</b> activations left.", activationService.RetrieveActivationCount(ExtendedProductId.Text));
+
+                    } else {
+                        Result.Text = "Please fill in all required fields and try again.";
+                    }
+                }
+
+            } catch (Exception ex) {
+                Result.Text = string.Format("The data could not be retrieved. {0}.", ex.Message);
+            }
+        }
     }
 }
