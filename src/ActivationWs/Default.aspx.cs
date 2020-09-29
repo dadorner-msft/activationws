f2b7bf47ea9006aea9e5f8866c93423d50eb0529
dadorner-msft <github@dorner-teo.de>
2020-09-29 22:32:56 +0200

    Update Default.aspx.cs

--- src/ActivationWs/Default.aspx.cs
+++ src/ActivationWs/Default.aspx.cs
@@ -15,16 +15,14 @@ namespace ActivationWs
 
         protected void Submit_Click(Object sender, EventArgs E) {
             try {
-                using (ActivationService activationService = new ActivationService()) {
-                    if (RadioButtonList1.SelectedIndex == 0 && !((string.IsNullOrWhiteSpace(InstallationId.Text)) || (string.IsNullOrWhiteSpace(ExtendedProductId.Text)))){
-                        Result.Text = string.Format("The Confirmation ID is: <b>{0}</b>", activationService.AcquireConfirmationId(InstallationId.Text, ExtendedProductId.Text));
+                if (RadioButtonList1.SelectedIndex == 0 && !((string.IsNullOrWhiteSpace(InstallationId.Text)) || (string.IsNullOrWhiteSpace(ExtendedProductId.Text)))) {
+                    Result.Text = string.Format("The Confirmation ID is: <b>{0}</b>", ActivationHelper.CallWebService(1, InstallationId.Text, ExtendedProductId.Text));
 
-                    } else if (RadioButtonList1.SelectedIndex == 1 && !(string.IsNullOrWhiteSpace(ExtendedProductId.Text))) {
-                        Result.Text = string.Format("You have <b>{0}</b> activations left.", activationService.RetrieveActivationCount(ExtendedProductId.Text));
+                } else if (RadioButtonList1.SelectedIndex == 1 && !(string.IsNullOrWhiteSpace(ExtendedProductId.Text))) {
+                    Result.Text = string.Format("You have <b>{0}</b> activations left.", ActivationHelper.CallWebService(2, null, ExtendedProductId.Text));
 
-                    } else {
-                        Result.Text = "Please fill in all required fields and try again.";
-                    }
+                } else {
+                    Result.Text = "Please fill in all required fields and try again.";
                 }
 
             } catch (Exception ex) {
